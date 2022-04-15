f5c8ff7543ffc23111dde14f1946baef857ddb02
suryo182 <suryo182@gmail.com>
2022-04-15 10:49:05 +0700

    Add sorter on every column

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -6,23 +6,35 @@ const columns = [
     title: 'Name',
     dataIndex: 'name',
     key: 'name',
+    sorter: (a, b) => {
+      return a.name.first.localeCompare(b.name.first);
+    },
     render: (text) => <span>{`${text.title} ${text.first} ${text.last}`}</span>,
   },
   {
     title: 'Gender',
     dataIndex: 'gender',
     key: 'gender',
+    sorter: (a, b) => {
+      return a.gender.localeCompare(b.gender);
+    },
   },
   {
     title: 'Location',
     dataIndex: 'location',
     key: 'location',
+    sorter: (a, b) => {
+      return a.location.city.localeCompare(b.location.city);
+    },
     render: (text) => <span>{text.city}</span>,
   },
   {
     title: 'Email',
     key: 'email',
     dataIndex: 'email',
+    sorter: (a, b) => {
+      return a.email.localeCompare(b.email);
+    },
   },
 ];
 
